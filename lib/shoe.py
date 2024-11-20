3f0c8564ce79211f557b2989b1d1c4237da94c80
janine pamintuan <jaja.ninnin@gmail.com>
2024-11-20 18:58:50 -0500

    book and shoe

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+    
+    size = property(get_size, set_size)
+
+    def get_condition(self):
+        return self._condition
+        
+    def set_condition(self, condition):
+        if isinstance(condition, str):
+            self._condition = condition
+        else:
+            raise TypeError("Condition is not a string")
+
+    condition = property(get_condition, set_condition)
+    
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.set_condition("New")
+        
